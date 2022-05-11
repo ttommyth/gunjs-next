29b4d9ba706d392faff4169c57161e012f49cfd8
ttommyth <ttommyth@gmail.com>
2022-05-11 23:04:17 +0800

    updated debug

--- src/lib/hooks/GunHook.tsx
+++ src/lib/hooks/GunHook.tsx
@@ -10,7 +10,7 @@ export const GunProvider = (props: { children?: React.ReactNode }) => {
     const [handgun, setHandgun] = useState<IGunInstance | null>(null);
     useEffect(() => {
         console.debug("setup handgun")
-        setHandgun(Gun(['http://localhost:3000/gun']));
+        setHandgun(Gun( [`${window.location.origin}/gun`]));
     }, [])
     return <GunContext.Provider value={{
         gun: handgun
