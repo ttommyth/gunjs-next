ecc14d38f36fb58056e13d3acda5bbff4efc89a2
ttommyth <ttommyth@gmail.com>
2022-05-12 00:18:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-import express, { Request, Response } from "express";
-import next from "next";
-
 require('gun/axe');
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+const express = require('express')
 const Gun = require('gun');
 
-const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev });
-const handle = nextApp.getRequestHandler();
-const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+// when using middleware `hostname` and `port` must be provided below
+const nextApp = next({ dev, hostname, port })
+const handle = nextApp.getRequestHandler()
 
-(async () => {
-  try {
-    await nextApp.prepare();
-    const app = express();
-    app.use(Gun.serve);
-    app.use(express.static('/gun'));
-    
-    app.all("*", (req, res) => {
-      return handle(req, res);
-    });
-    var server = app.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
-    });
-    var gun = Gun({	file: 'data', web: server });
-    // @ts-ignore
-    global.Gun = Gun; /// make global to `node --inspect` - debug only
-    // @ts-ignore
-    global.gun = gun; /// make global to `node --inspect` - debug only
-    console.log('Server started on port ' + port + ' with /gun');
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-})();
+
+nextApp.prepare().then(() => {
+  const app = express();
+  app.use(Gun.serve);
+  app.use(express.static('/gun'));
+  
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
+  var server = app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+  });
+  
+  var gun = Gun({	file: 'data', web: server });
+})
